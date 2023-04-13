d1b3e6f5241a83e869614b80a6470e878ec10825
munhunger <marcusmunger@gmail.com>
2023-04-13 18:51:47 +0200

    touchup on editor and build to gh-pages

--- editor/src/routes/+page.svelte
+++ editor/src/routes/+page.svelte
@@ -4,15 +4,15 @@
   import Editor from "./editor.svelte";
   console.log(kanlang);
   let kanlangExample = `          
-fn celsiusToFahrenheit(celsius Celsius alias num) Fahrenheit alias num {
-    num fahrenheit = celsius * 9 / 5 + 32
-    return fahrenheit
-}
-Celsius c = 100
-Fahrenheit f
-fn main() DI alias num {
-    return f
-}`;
+  fn celsiusToFahrenheit(celsius Celsius alias num) Fahrenheit alias num {
+      num fahrenheit = celsius * 9 / 5 + 32
+      return fahrenheit
+  }
+  Celsius c = 100
+  Fahrenheit f
+  fn main() DI alias num {
+      return f
+  }`;
   // @ts-ignore
   function compile(input) {
     try {
@@ -24,6 +24,23 @@ fn main() DI alias num {
   $: jsOutput = compile(kanlangExample);
 </script>
 
+<div>
+  <h1>Kanlang Editor</h1>
+  <p>This is a simple editor for the Kanlang programming language.</p>
+  <p>It is only meant as a pre MVP test.</p>
+  <p>
+    On the left you have your kanlang code, and on the right you have the
+    compiled javascript
+  </p>
+  <p>
+    This is far from done or even working, but I welcome you to play with it
+  </p>
+  <p>
+    Read more about it on <a href="https://github.com/munHunger/kanlang"
+      >github</a
+    >
+  </p>
+</div>
 <div id="container">
   <div>
     <Editor bind:code={kanlangExample} language="kanlang" />
--- editor/src/routes/editor.svelte
+++ editor/src/routes/editor.svelte
@@ -1,60 +1,21 @@
 <script lang="ts">
-  import type monaco from "monaco-editor";
-  import { onMount } from "svelte";
-  import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-  import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-  import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-  import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-  import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
-
   export let code: string = "";
   export let language: string = "javascript";
 
   export let listen: boolean = false;
-
-  let divEl: HTMLDivElement = null;
-  let editor: monaco.editor.IStandaloneCodeEditor;
-  let Monaco;
-
-  $: ((code: string) => {
-    if (editor && listen) {
-      editor.setValue(code);
-    }
-  })(code);
-  onMount(async () => {
-    // @ts-ignore
-    self.MonacoEnvironment = {
-      getWorker: function (_moduleId: any, label: string) {
-        if (label === "json") {
-          return new jsonWorker();
-        }
-        if (label === "css" || label === "scss" || label === "less") {
-          return new cssWorker();
-        }
-        if (label === "html" || label === "handlebars" || label === "razor") {
-          return new htmlWorker();
-        }
-        if (label === "typescript" || label === "javascript") {
-          return new tsWorker();
-        }
-        return new editorWorker();
-      },
-    };
-
-    Monaco = await import("monaco-editor");
-    editor = Monaco.editor.create(divEl, {
-      value: code,
-      language,
-    });
-
-    editor.onKeyUp(() => {
-      code = editor.getValue();
-    });
-
-    return () => {
-      editor.dispose();
-    };
-  });
 </script>
 
-<div bind:this={divEl} style="height: 100%" />
+<textarea bind:value={code} />
+
+<style>
+  textarea {
+    width: 100%;
+    height: 100vh;
+    border: none;
+    background: none;
+    font-family: monospace;
+    font-size: 1.2em;
+    outline: none;
+    padding: 0em;
+  }
+</style>
