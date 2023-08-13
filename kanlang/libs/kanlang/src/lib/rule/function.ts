6616d2ad9cf09c83e07d94b840f660f95097a774
munhunger <marcusmunger@gmail.com>
2023-08-13 22:56:53 +0200

    added function array

--- kanlang/libs/kanlang/src/lib/rule/function.ts
+++ kanlang/libs/kanlang/src/lib/rule/function.ts
@@ -1,3 +1,5 @@
+import { State } from '../earley';
+import { Token } from '../tokenizer';
 import { Body } from './body';
 import { NewRuleType, Rule } from './rule';
 
@@ -13,12 +15,54 @@ export class Function extends Rule {
         root: 0,
         parts: [
           ['punct', '('],
+          new ArgumentArray(),
           ['punct', ')'],
           ['punct', '{'],
           new Body(),
           ['punct', '}'],
         ],
         carryScope: false,
+      },
+    ];
+  }
+}
+
+export class ArgumentArray extends Rule {
+  get rules(): NewRuleType[] {
+    return [
+      {
+        root: 0,
+        parts: [this, ['punct', ','], new Argument()],
+        carryScope: true,
+      },
+      {
+        root: 0,
+        carryScope: true,
+        parts: [new Argument()],
+      },
+      {
+        root: 0,
+        parts: [], //epsilon rule
+      },
+    ];
+  }
+}
+
+export class Argument extends Rule {
+  get rules(): NewRuleType[] {
+    return [
+      {
+        root: 0,
+        parts: ['identifier', ['punct', ':'], 'identifier'],
+        carryScope: true,
+        semantic: (scope, state) => ({
+          name: (state.tree[0] as Token).value,
+          variable: {
+            constant: false,
+            primitive: true, //TODO: is it?
+            type: (state.tree[2] as Token).value,
+          },
+        }),
       },
     ];
   }
